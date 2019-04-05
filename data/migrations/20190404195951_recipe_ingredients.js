e059194be3fb4e87f4f7aae91c4b0ee139f708f6
iradsanchez <ira.d.sanchez@gmail.com>
2019-04-04 20:13:23 -0700

    Add Recipe Ingredients Migration

--- data/migrations/20190404195951_recipe_ingredients.js
+++ data/migrations/20190404195951_recipe_ingredients.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("recipe_ingredients", function(tbl) {
+    tbl
+      .integer("dish_id")
+      .notNullable()
+      .references("id")
+      .inTable("dishes")
+      .onDelete("CASCADE");
+    tbl
+      .integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE");
+    tbl.primary(["dish_id", "recipe_id"]);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("recipe_ingredients");
 };
